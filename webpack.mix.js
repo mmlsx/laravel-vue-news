ef64d1703020aca942d3540a3111f2fb66fdcfc2
mml <sxlmm93@163.com>
2018-03-18 19:57:03 +0800

    更新列表页，局部刷新代替整体页面刷新

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/assets/js/app.js', 'public/js')
    .extract(['vue', 'vue-router', 'axios', 'vuex'])
    .sass('resources/assets/sass/app.scss', 'public/css');
